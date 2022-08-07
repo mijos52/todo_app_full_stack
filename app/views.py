9fc8b36b3e105c7c02c2982ced4565e838bc39d2
ma jo <styleethnic1@gmail.com>
2022-08-07 23:05:06 +0530

    Basic structure done

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from django.views.generic.list import ListView
 from .models import Todo
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.views.generic.edit import UpdateView, DeleteView
 
 
 class TaskList(ListView):
@@ -15,3 +18,27 @@ class TaskDetail(DetailView):
 
     model = Todo
     context_object_name = "task"
+
+
+class TaskCreate(CreateView):
+    """Create a new todo item"""
+
+    model = Todo
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
+
+
+class TaskUpdate(UpdateView):
+    """Update an exit todo item"""
+
+    model = Todo
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
+
+
+class TaskDelete(DeleteView):
+    """Delete an item from the todo list"""
+
+    model = Todo
+    fields = "__all__"
+    success_url = reverse_lazy("tasks")
